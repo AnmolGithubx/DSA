132af025ac14e58d2a1425477124849c1b8fcfd9
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-07-23 22:25:36 +0530

    Time: 0 ms (100%), Space: 44.4 MB (61.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,21 +11,21 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
+        if(head == null || head.next == null){ // agr list empty hai ya sirf ek hi node hai, then cycle is not possible
             return false;
         }
 
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode slow = head; // ek step se chlega
+        ListNode fast = head; // 2 step se chlega ,, bcz agr cycle hua toh yeh toh match kr hi jayenge ek point pr aake
 
-        while(fast != null && fast.next != null){
+        while(fast != null && fast.next != null){ // jbb tk yeh dono null nhi hote tbb tk loop chle bcz agr yeh null ho gye mtlb cycle mil gya
             slow = slow.next;
-            fast = fast.next.next;
+            fast = fast.next.next; // movements of nodes by 1 and 2
 
             if(slow == fast){
                 return true;
             }
         }
-        return false;
+        return false; // and agr cycle nhi hai toh false kr de 
     }
 }
