7e4c16183c808efc9626eed6a46071ba1913f30a
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-07-23 11:38:35 +0530

    Time: 2 ms (98.01%), Space: 45.1 MB (37.6%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int[] result = new int[nums.length];
+        int idx=0;
+        for(int i=0;i<nums.length;i+=2){
+            int alice=nums[i];
+            int bob = nums[i+1];
+            result[idx++] = bob;
+            result[idx++] = alice;
+        }
+        return result;
+        // DONE BY TAKING 2 VARIABLE FOR ALICE AND BOB
+    }
+}
