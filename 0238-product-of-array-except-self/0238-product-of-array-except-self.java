3bcea3093ab8a8bf181418e751b9afd171ffc730
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-08-23 20:40:31 +0530

    Time: 2 ms (87.09%), Space: 55.4 MB (74.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,14 +4,14 @@ class Solution {
         int[] answer = new int[n];
 
         //here we will take the product of all elements before the current index
-        answer[0] = 1;
-        for (int i = 1; i < n; i++) { 
-            answer[i] = answer[i - 1] * nums[i - 1];
+        answer[0] = 1; 
+        for (int i = 1; i < n; i++) { // 0 index de left ch kuj haini ess krke 1 tonh shuru krange
+            answer[i] = answer[i - 1] * nums[i - 1]; // Yeh har element ke liye uske left side ke numbers ka product store kar raha hai.
         }
 
         //and here we will calcuate the product of all elements after the current index), and multiply it with the prefix result.
         int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) { //Yeh loop right se left jaa raha hai: answer[i] ke andar already left ka product hai.Ab usme right ka product bhi multiply kar dete hain.
             answer[i] *= right;
             right *= nums[i];
         }
