70c44c819a72c7472e4177410c1722de8050cab3
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-09-12 09:32:30 +0530

    Time: 2 ms (100%), Space: 46.2 MB (6.04%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        String lowerCaseString = s.toLowerCase();
-        
-        for (char c : lowerCaseString.toCharArray()) {
-
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                return true;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'a','e','i','o','u':
+                    return true;
             }
         }
+
         return false;
     }
 }
