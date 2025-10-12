729acb0e2c650200a9727acae714fc43df0c67c8
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-10-12 16:54:05 +0530

    Time: 1 ms (99.68%), Space: 44.6 MB (12.18%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int compress(char[] chars) {
+        int writeIndex = 0, readIndex = 0;
+        while (readIndex < chars.length) {
+            char currentChar = chars[readIndex];
+            int count = 0;
+            while (readIndex < chars.length && chars[readIndex] == currentChar) {
+                readIndex++;
+                count++;
+            }
+            chars[writeIndex++] = currentChar;
+            if (count > 1) {
+                for (char digit : String.valueOf(count).toCharArray()) {
+                    chars[writeIndex++] = digit;
+                }
+            }
+        }
+        return writeIndex;
+    }
+}
