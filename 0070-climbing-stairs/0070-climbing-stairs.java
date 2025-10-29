aa99cb44e9e6152e0cd613cb57dd956d7d92ec5a
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-10-29 20:55:17 +0530

    Time: 0 ms (100%), Space: 40.6 MB (40.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,18 @@
 class Solution {
+    int[] dp;
     public int climbStairs(int n) {
-        if (n <= 2) return n;
-        
-        int first = 1, second = 2;
-        for (int i = 3; i <= n; i++) {
-            int third = first + second;
-            first = second;
-            second = third;
-        }
-        
-        return second;
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return rec(n);
+    }
+    public int rec(int remaining){
+        if(remaining == 0) return 1;
+        if(remaining == -1) return 0;
+        if(dp[remaining] != -1) return dp[remaining];
+
+        int one_step = rec(remaining-1);
+        int two_step = rec(remaining-2);
+        dp[remaining] = one_step + two_step;
+        return one_step+two_step;
     }
 }
