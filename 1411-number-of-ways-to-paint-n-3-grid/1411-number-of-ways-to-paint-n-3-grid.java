64bfed23f045b545d2d896d834a0a88eee8aeef9
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2026-01-03 20:51:44 +0530

    Time: 3 ms (93.85%), Space: 42.1 MB (61.54%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numOfWays(int n) {
+        long mod = 1_000_000_007;
+        
+        // Initial counts for n = 1
+        long aba = 6;
+        long abc = 6;
+        
+        for (int i = 2; i <= n; i++) {
+            long nextAba = (aba * 3 + abc * 2) % mod;
+            long nextAbc = (aba * 2 + abc * 2) % mod;
+            
+            aba = nextAba;
+            abc = nextAbc;
+        }
+        
+        return (int) ((aba + abc) % mod);
+    }
+}
