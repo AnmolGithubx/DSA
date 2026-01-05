3d6add975a6d0d83e69a3f4c359e95150c9f7386
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2026-01-05 16:01:42 +0530

    Time: 3 ms (100%), Space: 68.1 MB (52.73%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,26 +1,26 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        long sum = 0;
-        int negCount = 0;
-        int minAbs = Integer.MAX_VALUE;
+        int n = matrix.length;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                int val = matrix[i][j];
+        int leastElement = 100001;
+        long sum = 0L;
+        int negativeCount = 0;
 
-                if (val < 0) negCount++;
+        for(int[] rows : matrix ){
+            for(int value : rows){
+                if(value < 0){
+                    negativeCount++;
+                    value = -value;
+                }
 
-                int absVal = Math.abs(val);
-                sum += absVal;
-                minAbs = Math.min(minAbs, absVal);
+                sum += value;
+                if(value < leastElement){
+                    leastElement = value;
+                }
+                
             }
         }
 
-        // If odd negatives, one smallest value must stay negative
-        if (negCount % 2 != 0) {
-            sum -= 2L * minAbs;
-        }
-
-        return sum;
+        return (negativeCount%2 == 0) ? sum : (sum - 2*leastElement);
     }
 }
