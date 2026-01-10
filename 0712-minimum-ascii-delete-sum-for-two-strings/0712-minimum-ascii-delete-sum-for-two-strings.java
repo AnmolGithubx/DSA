45b48e911ee98809ab94e596d446aa03aa632385
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2026-01-10 22:29:38 +0530

    Time: 7 ms (100%), Space: 46.5 MB (58.72%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,35 +1,21 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        int[][] dp = new int[m + 1][n + 1];
-
-        // Base case: s2 is empty, delete all chars of s1
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
-        }
-
-        // Base case: s1 is empty, delete all chars of s2
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
-        }
-
-        // Fill the DP table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    // Characters match, no cost added
-                    dp[i][j] = dp[i - 1][j - 1];
+        char[] a = s1.toCharArray(); char[] b = s2.toCharArray();
+        int n = a.length; int m = b.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int total = 0;
+        for(char c: a) total += (int) c;
+        for(char c: b) total += (int) c;
+        for(int i =  n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                if(a[i] == b[j]){
+                    dp[i][j] += dp[i + 1][j + 1] + (int) a[i];
                 } else {
-                    // Characters don't match, take the minimum cost of deleting from either string
-                    dp[i][j] = Math.min(
-                        dp[i - 1][j] + s1.charAt(i - 1), 
-                        dp[i][j - 1] + s2.charAt(j - 1)
-                    );
+                    dp[i][j] = Math.max(dp[i +1][j], dp[i][j + 1]);
                 }
+
             }
         }
-
-        return dp[m][n];
+        return -dp[0][0] * 2 + total;
     }
 }
