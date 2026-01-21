260afcd396569caa4cd30b36cfcc3c1c05048bc4
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2026-01-21 10:42:26 +0530

    Time: 1 ms (100%), Space: 46.8 MB (75%) - LeetHub

---
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int val = nums.get(i);
+            
+            if (val == 2) {
+                ans[i] = -1;
+            } else {
+                int temp = val;
+                int count = 0;
+                while ((temp & 1) == 1) {
+                    temp >>= 1;
+                    count++;
+                }
+                ans[i] = val ^ (1 << (count - 1));
+            }
+        }
+        
+        return ans;
+    }
+}
